32a8c0669081a746b55f9ba26761b5ba074ff3bc
AZWebDDS <az.webdds@gmail.com>
2018-03-27 18:13:41 +0300

    Сhore (1 file): Dev continue (NoIssue)
    ОБЫЧНОЕ НАПИСАНИЕ КОДА
    
    Добавлены и описаны текстовые переменные, отработан механизм
    добавления каталогов и файлов, внесения записи в httpd-vhosts.conf и
    файл %windows%\... \hosts
    
    Выполняется текущая разработка
    e:\www\2018\SANDBOX\BASH\NewWebProjectLocCreator\new-web-project-loc-creator.ps1

--- new-web-project-loc-creator.ps1
+++ new-web-project-loc-creator.ps1
@@ -1,5 +1,75 @@
 ﻿cls
+
+## # Переменные сеанса:
+
+# Код, который записывается в index.html файл:
+
+$index_html_code = 
+'<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PROJECT_NAME</title>
+</head>
+
+<body>
+
+    <div class="">Hello World!</div>
+    <div class="">Далее какой-то текст...</div>
+
+</body>
+
+</html>'
+
+$index_php_code = 
+"
+<?php
+/**
+ * PHP Version 7
+ * Начальный index-файл (заглушка)
+ * @author   AZWebDDS <AZWebDDS@gmail.com>
+ * @link     http://....
+ */
+ 
+echo '<h1>Hello World!</h1>';
+echo '<div style=`"color: green`">Ваш новый локальный хост (сайт) готов!</div>';
+echo '<div><a href=`"http://$project_name`">http://$project_name</a></div>';
+
+phpijnbfo();
+"
+
+$end_to_vhosts_file = 
+"
+    # Вирт. хост http://$project_name
+    <VirtualHost *:80>
+        ServerAdmin webmaster@$project_name
+        DocumentRoot `"e:/www/2018/SANDBOX/PHP/$project_name/www`"
+        ServerName $project_name
+        ErrorLog `"e:/www/2018/SANDBOX/PHP/$project_name/logs/error.log`"
+        CustomLog `"e:/www/2018/SANDBOX/PHP/$project_name/logs/access.log`" common
+    </VirtualHost>
+    <Directory `"e:/www/2018/SANDBOX/PHP/$project_name/www`">
+        AllowOverride All
+        Require all granted
+    </Directory>"
+
+
+
+## # Функции сеанса
+
+# Переход в каталог проектов
+function cd_catalog() {
+cd $path_to_project
+}
+
+
+
 ## Приветствие
+
+
+
+
 
 ''
 '      Hello World!'
@@ -61,13 +131,59 @@ Write-Host ' Проект будет создан в каталоге ' -Foregro
 Write-Host $path_to_project -ForegroundColor Green
 ''
 Write-Host '     Это верно?' -ForegroundColor Yellow -NoNewline
-'   Y/n'
+Write-Host '  Y/n:  ' -NoNewline
+$yes_not = Read-Host
+''
 '_______________________________________________'
+
 
 ''
 
+## Ветвление Yes|NO
+
+if ($yes_not -Match "y|Y|д|Д")
+{
+    cd_catalog($path_to_project)
+    # Создать каталог и перейти в него
+    # Создать файл в этом новом каталоге
+
+    $project_name = Read-Host ' Введите имя директории  проекта (название проекта)'
+    New-Item . -Name $project_name -ItemType Directory
+    cd ./$project_name
+
+    New-Item . -Name _PROJECT_ -ItemType Directory
+    New-Item . -Name logs -ItemType Directory
+    New-Item . -Name ThisProject.todo -ItemType File
+
+
+    New-Item . -Name www -ItemType Directory
+    cd ./www
+    New-Item . -Name .gitignore -ItemType File
+    New-Item . -Name .htaccess -ItemType File
+    New-Item . -Name index.html -ItemType File -Value $index_html_code
+    New-Item . -Name index.php -ItemType File -Value $index_php_code
+    New-Item . -Name README.md -ItemType File
+    
+    dir
+
+    ## Создание самого хоста на сервере
+    $end_to_vhosts_file | Out-File -FilePath f:\2018\DevProg\WebDev\XAMPP-722-0-VC15\apache\conf\extra\httpd-vhosts.conf -Append -Encoding utf8
+    
+    $end_to_hosts_file = Write-Host '127.0.0.1' $($project_name)
+    Start-Process powershell "-NoExit -Command `"Add-Content -Path 'c:\Windows\System32\drivers\etc\hosts' -Value `"$end_to_hosts_file`"`"" -Verb RunAs
+
+    Start-Sleep -Seconds 5
+
+
+
+
+    Pause
+
+}
+else
+{
+    'ни то ни се'
+}
+
+''
 Pause
-
-
-
-# set /p NEW_HOST_DIR=
